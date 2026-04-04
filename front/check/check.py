2067993f76c4f7fbbf64a1562977620b931f129e
user <vadim231930@yandex.ru>
2026-04-04 17:04:56 +0300

    ДОДЕЛАЛ САЙТ

--- front/check/check.py
+++ front/check/check.py
@@ -2,11 +2,7 @@ import os
 from flask import Flask, render_template
 from back.database.database import create_database
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-project_root = os.path.abspath(os.path.join(current_dir, '..', '..', '..'))
-static_dir = os.path.join(project_root, 'static')
-
-app = Flask(__name__, static_folder=static_dir)
+app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
