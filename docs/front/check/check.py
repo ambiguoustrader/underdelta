0f36d828f72a4c06ec5efef2cdcc07f8ca951523
LKLK <vadimivanov4970@gmail.com>
2026-04-04 15:50:40 +0300

    доработать

---
+++ docs/front/check/check.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask, render_template
+from docs.back.database.database import create_database
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.abspath(os.path.join(current_dir, '..', '..', '..'))
+static_dir = os.path.join(project_root, 'static')
+
+app = Flask(__name__, static_folder=static_dir)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = "Ученик Яндекс.Лицея"
+    return render_template('base.html')
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1', debug=True)
